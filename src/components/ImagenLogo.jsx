01d0efc0776e4944c485b54ef5bfba05e09acae8
Stefano <=>
2022-02-07 16:40:52 -0500

    agregando botnes al sidebar y con responsive de css

---
+++ src/components/ImagenLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from 'media/logo.png'
+
+const ImagenLogo = () => {
+  return <img className='mx-auto h-52 w-auto' src={logo} alt='Rayo'/>
+};
+
+export default ImagenLogo;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import TriggerDarkMode from './TriggerDarkMode';
-
+import { Link } from 'react-router-dom';
+import ImagenLogo from './ImagenLogo';
+import 'styles/responsive.css'
+/*Uno debe garantizar que la menos se vea en 300px */
 const Sidebar = () => {
   return (
-    <nav className='w-72 bg-red-400'>
-      <TriggerDarkMode/>
-    </nav>
+    <nav className='w-72 bg-red-400 h-full flex flex-col border border-gray-500 p-4 sidebar'>
+
+      <Link to='admin' >
+        <ImagenLogo />
+      </Link>
+      <div className='my-4'>
+
+        <Ruta icono='fas fa-user' ruta='admin/profile' nombre='Perfil' />
+        <Ruta icono='fas fa-car' ruta='admin/vehiculos' nombre='Vehículos' />
+        <Ruta icono='fas fa-cash-register' ruta='admin/ventas' nombre='Ventas' />
+        <Ruta icono='fas fa-users' ruta='admin/usuarios' nombre='Usuarios' />
+
+      </div>
+      <button>Cerrar sesión</button>
+
+    </nav >
   );
 };
+const Ruta = ({ icono, ruta, nombre }) => {
+  return (
+    <Link to={ruta}>
+      <button className='flex my-2 hover:bg-indigo-900 p-1 bg-indigo-300 w-full items-center text-white rounded-md'>
+        <i className={`${icono} w-10`} />
+        {nombre}
+      </button>
+    </Link>
+  )
+}
 
 export default Sidebar;
